c86e55a53556ec92419cdb2ff1272976c7808de8
Sahil Yousaf <sahil.yousaf@students.iaac.net>
2026-01-03 00:44:18 +0100

    Aggressive memory optimizations for Render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false
+    sourcemap: false,
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 1000
   }
 })
